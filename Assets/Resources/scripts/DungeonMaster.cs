46283c56fcb1cddc99c2b43f2147587c7a93356e
freigus <andrey2001v@gmail.com>
2021-03-29 23:27:49 +1000

    save/load system finished

--- Assets/Resources/scripts/DungeonMaster.cs
+++ Assets/Resources/scripts/DungeonMaster.cs
@@ -25,6 +25,7 @@ public class DungeonMaster : MonoBehaviour
 
     //dict that says which relates object to its type
     List<block_info> all_blocks = new List<block_info>(); //for save system
+    string save_path = "Saves\\save.sav"; //self-explainatory
 
 
     // Start is called before the first frame update
@@ -62,19 +63,7 @@ public class DungeonMaster : MonoBehaviour
                     //h_transformed.y += 0.5f * block_scale;
                     if (Input.GetButtonDown("Fire1"))
                     {
-                        GameObject real_block = Instantiate(block_prefab);
-                        //scaling
-                        real_block.transform.localScale = block_prefab.transform.localScale * block_scale;
-                        //rotation
-                        Vector3 angles = real_block.transform.rotation.eulerAngles;
-                        real_block.transform.rotation = Quaternion.Euler(
-                                            angles.x,
-                                            rotation_pos * 90,
-                                            angles.z);
-                        //place it on pointer
-                        real_block.transform.position = h_real;
-                        all_blocks.Add(new block_info(real_block, cur_block_id));
-                        Debug.Log("There are " + all_blocks.Count.ToString() + "in all_blocks");
+                        create_block(h_real);
                     }
                     else
                     {
@@ -83,7 +72,6 @@ public class DungeonMaster : MonoBehaviour
                     }
                 }
             }
-
             else if (is_delete_mode)
             {
                 RaycastHit hit;
@@ -141,8 +129,6 @@ public class DungeonMaster : MonoBehaviour
                 }
             }
         }
-        
-
     }
 
     public void on_build_btn_click()
@@ -269,7 +255,6 @@ public class DungeonMaster : MonoBehaviour
     public void on_save_btn_click()
     {
         //rewrite save if already exists
-        string save_path = "Saves\\save.sav";
         if (File.Exists(save_path))
         {
             File.WriteAllText(save_path, "");
@@ -281,25 +266,26 @@ public class DungeonMaster : MonoBehaviour
             String to_save = "";
             block_info info = all_blocks[i];
 
-            //save type of block
+            //save type of block 0
             to_save += info.block_type.ToString() + " ";
 
-            //save xyz coordinates
+            //save xyz coordinates 1 2 3
             Vector3 pos = info.block.transform.localPosition;
             to_save += pos.x + " " + pos.y + " " + pos.z + " ";
 
-            //save xyz in eulerAngles of rotation
-            Vector3 rot = info.block.transform.rotation.eulerAngles;
-            to_save += rot.x + " " + rot.y + " " + rot.z + " ";
+            //save xyzw of Quaternion 4 5 6 7
+            Vector3 rot_t = info.block.transform.rotation.eulerAngles;
+            Vector3 rot = new Vector3(Round(rot_t.x), Round(rot_t.y), Round(rot_t.z));
+            to_save += rot.x + " " + rot.y + " " + rot.z + " " + 1 + " ";
 
-            //save xyz scale
+            //save xyz scale 8 9 10
             Vector3 scl = info.block.transform.localScale;
             to_save += scl.x + " " + scl.y + " " + scl.z + " ";
 
-            //save rgb of colors
+            //save rgb of colors 11 12 13
             Renderer rend = info.block.transform.GetComponent<Renderer>();
             Color col = rend.material.color;
-            to_save += col.r + " " + col.g + " " + col.b + " ";
+            to_save += col.r + " " + col.g + " " + col.b;
 
             //add new_line character
             to_save += Environment.NewLine;
@@ -309,4 +295,122 @@ public class DungeonMaster : MonoBehaviour
 
         }
     }
+
+    public void on_load_btn_click()
+    {
+
+        //check if save is there
+        if (!File.Exists(save_path))
+        {
+            Debug.Log("couldnt locate Save\\Save.sav");
+            return;
+        }
+
+        //delete all blocks before loading
+        for (int i = 0; i < all_blocks.Count;)
+        {
+            GameObject block = all_blocks[i].block;
+            Destroy(block);
+            all_blocks.RemoveAt(i);
+
+        }
+
+        var numberFormatInfo = new System.Globalization.NumberFormatInfo();
+        numberFormatInfo.NumberDecimalSeparator = ",";
+
+
+        int block_type;
+        Vector3 pos = new Vector3();
+        Vector3 rot = new Vector3();
+        Vector3 scl = new Vector3();
+        Color col = new Color();
+
+        try
+        {
+            using (StreamReader file = new StreamReader(save_path))
+            {
+                string line;
+                
+                while ((line = file.ReadLine()) != null)
+                {
+                    //create all block vars
+                    string[] vars_str = line.Split(' ');
+
+                    foreach (string str in vars_str)
+                    {
+                        Debug.Log(str);
+                    }
+
+                    block_type = int.Parse(vars_str[0]);
+
+                    //Debug.Log("Processing pos...");
+                    pos.x = float.Parse(vars_str[1], numberFormatInfo);
+                    pos.y = float.Parse(vars_str[2], numberFormatInfo);
+                    pos.z = float.Parse(vars_str[3], numberFormatInfo);
+                    //Debug.Log(pos.ToString());
+
+                    //Debug.Log("Processing rot...");
+                    rot.x = float.Parse(vars_str[4], numberFormatInfo);
+                    rot.y = float.Parse(vars_str[5], numberFormatInfo);
+                    rot.z = float.Parse(vars_str[6], numberFormatInfo);
+                    //rot.w = float.Parse(vars_str[7]); we dont use quaternions here. vars_str[7] is always 0
+                    //Debug.Log(rot.ToString());
+
+                    //Debug.Log("Processing scl...");
+                    scl.x = float.Parse(vars_str[8], numberFormatInfo);
+                    scl.y = float.Parse(vars_str[9], numberFormatInfo);
+                    scl.z = float.Parse(vars_str[10], numberFormatInfo);
+                    //.Log(scl.ToString());
+
+                    //Debug.Log("Processing col...");
+                    col.r = float.Parse(vars_str[11], numberFormatInfo);
+                    col.g = float.Parse(vars_str[12], numberFormatInfo);
+                    col.b = float.Parse(vars_str[13], numberFormatInfo);
+                    col.a = 1;
+                    //Debug.Log(col.ToString());
+
+                    //Debug.Log("Finished processing.");
+
+                    block_prefab = (GameObject)block_list[block_type];
+                    GameObject block = create_block(pos);
+                    block.transform.rotation = Quaternion.Euler(rot);
+                    block.transform.localScale = scl;
+                    Renderer rend = block.transform.GetComponent<Renderer>();
+                    rend.material.color = col;
+
+
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            // Let the user know what went wrong.
+            Debug.Log("The file could not be read:");
+            Debug.Log(e.Message);
+            return;
+        }
+
+
+
+
+    }
+
+    GameObject create_block(Vector3 pos)
+    {
+        GameObject real_block = Instantiate(block_prefab);
+        //scaling
+        real_block.transform.localScale = block_prefab.transform.localScale * block_scale;
+        //rotation
+        Vector3 angles = real_block.transform.rotation.eulerAngles;
+        real_block.transform.rotation = Quaternion.Euler(
+                            angles.x,
+                            rotation_pos * 90,
+                            angles.z);
+        //place it on pointer
+        real_block.transform.position = pos;
+        all_blocks.Add(new block_info(real_block, cur_block_id));
+        Debug.Log("There are " + all_blocks.Count.ToString() + "in all_blocks");
+        return real_block;
+    }
+
 }
