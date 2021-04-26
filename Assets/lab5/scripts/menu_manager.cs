360cc2e6a27afbc12119673054f14c0733540c6e
freigus <andrey2001v@gmail.com>
2021-04-26 15:49:20 +1000

    lab6 start

--- Assets/lab5/scripts/menu_manager.cs
+++ Assets/lab5/scripts/menu_manager.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class bullet_behaviour : MonoBehaviour
+public class menu_manager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, 10f);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void onStartButton()
+    {
+        SceneManager.LoadScene("lab5_scene");
     }
 }
